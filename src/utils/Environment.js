8b0185404af9bcb9037127c1d8af1b5cc14ccf2e
Sardar Shahzeb <shahzeb.naseer2@gmail.com>
2021-10-14 19:01:10 +0500

    url change

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -1,6 +1,6 @@
 let Environment = {
-    // backendUrl : "https://api.nftarmy.app"
-    backendUrl : "http://192.168.18.40:38451"
+    backendUrl : "https://api.nftarmy.app"
+    // backendUrl : "http://192.168.18.40:38451"
     // http://54.191.140.38:38451
   
   };
